b1440226160e67cee4c99095d90c701600c16e1e
MercyAngelin <mercyangelin2000@gmail.com>
2022-09-26 19:04:10 +0530

    commit

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,57 +1,57 @@
-# from fastapi.testclient import TestClient
-# from Controller.main import app
-# from Database.database import get_db,Base
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker 
-# import pytest
-# from Configuration.config import setting
-# from Authentication.oauth2 import create_access_token
+from fastapi.testclient import TestClient
+from Controller.main import app
+from Database.database import get_db,Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker 
+import pytest
+from Configuration.config import setting
+from Authentication.oauth2 import create_access_token
 
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}_test"  #username:password@hostname/dbname
+SQLALCHEMY_DATABASE_URL = f"postgresql://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}_test"  #username:password@hostname/dbname
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# TestingSessionlocal = sessionmaker(autocommit=False, autoflush=False,bind=engine) 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionlocal = sessionmaker(autocommit=False, autoflush=False,bind=engine) 
 
-# @pytest.fixture
-# def session():
-#     # print(SQLALCHEMY_DATABASE_URL)
-#     Base.metadata.drop_all(bind=engine)
-#     Base.metadata.create_all(bind=engine)
-#     db = TestingSessionlocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+@pytest.fixture
+def session():
+    # print(SQLALCHEMY_DATABASE_URL)
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-# @pytest.fixture
-# def client(session):
-#     def override_get_db():
-#         try:
-#             yield session
-#         finally:
-#             session.close()
-#     app.dependency_overrides[get_db] = override_get_db
-#     yield TestClient(app)
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
 
-# @pytest.fixture
-# def test_user(client):
-#     user_data = {"firstname":"Mercy","lastname":"Angelin","phone":"8765432876","email":"mercy@gmail.com","dob":"2000-08-04","gender":"female","pwd":"mylu"}
-#     res = client.post("/register",json=user_data)
-#     print(res.json())
-#     new_user = res.json()
-#     new_user['password'] = user_data['pwd']
-#     return new_user
+@pytest.fixture
+def test_user(client):
+    user_data = {"firstname":"Mercy","lastname":"Angelin","phone":"8765432876","email":"mercy@gmail.com","dob":"2000-08-04","gender":"female","pwd":"mylu"}
+    res = client.post("/register",json=user_data)
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['pwd']
+    return new_user
 
-# @pytest.fixture
-# def token(test_user):
-#     return create_access_token({"user_id":test_user['id']})
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id":test_user['id']})
 
-# @pytest.fixture
-# def authorized_client(client,token):
-#     client.headers = {
-#         **client.headers,
-#         "Authorization":f"bearer {token}"
+@pytest.fixture
+def authorized_client(client,token):
+    client.headers = {
+        **client.headers,
+        "Authorization":f"bearer {token}"
        
-#     }
-#     return client
+    }
+    return client
 
--- TestCases/test_users.py
+++ TestCases/test_users.py
@@ -1,43 +1,43 @@
-# from Schema import schema
-# from jose import jwt
-# from Configuration.config import setting
-# import pytest
+from Schema import schema
+from jose import jwt
+from Configuration.config import setting
+import pytest
 
-# def test_getmethod(client):
-#     res = client.get("/")
-#     print(res.json().get("Hai"))
-#     assert res.json().get("Hai") == "Hai da"
+def test_getmethod(client):
+    res = client.get("/")
+    print(res.json().get("Hai"))
+    assert res.json().get("Hai") == "Hai da"
 
-# def test_login(client,test_user):
-#     res = client.post("/login",data={"username":test_user['email'],"password":test_user['password']})
-#     login_res = schema.Token(**res.json())
-#     payload = jwt.decode(login_res.access_token,setting.secret_key,setting.algorithm)
-#     id:str = payload.get("user_id")
-#     assert id == test_user['id']
-#     assert login_res.token_type == "bearer"
-#     assert res.status_code == 200
+def test_login(client,test_user):
+    res = client.post("/login",data={"username":test_user['email'],"password":test_user['password']})
+    login_res = schema.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,setting.secret_key,setting.algorithm)
+    id:str = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
 
-# @pytest.mark.parametrize("email,password,status_code,detail",[
-#     ('wrongemail@gmail.com','mylu',403,'Invalid Credentials'),
-#     ('mercy@gmail.com','uioruiouio',403,'Invalid Credentials'),
+@pytest.mark.parametrize("email,password,status_code,detail",[
+    ('wrongemail@gmail.com','mylu',403,'Invalid Credentials'),
+    ('mercy@gmail.com','uioruiouio',403,'Invalid Credentials'),
 
-# ])
-# def test_wrong_user(client,test_user,email,password,status_code,detail):
-#     res = client.post("/login",data={"username":email,"password":password})
-#     assert res.status_code == status_code
-#     assert res.json().get('detail')== detail
+])
+def test_wrong_user(client,test_user,email,password,status_code,detail):
+    res = client.post("/login",data={"username":email,"password":password})
+    assert res.status_code == status_code
+    assert res.json().get('detail')== detail
 
-# def test_get_all_posts(authorized_client):
-#     res = authorized_client.get("/userprofile/")
-#     print(res.json())
-#     assert res.status_code == 200
+def test_get_all_posts(authorized_client):
+    res = authorized_client.get("/userprofile/")
+    print(res.json())
+    assert res.status_code == 200
 
-# def test_update_posts(authorized_client):
-#     res = authorized_client.put("/up_userprofile/",json = {"firstname":"Mercy","lastname":"Angel","phone":"8778451528","email":"mercy@gmail.com","dob":"2000-08-04","gender":"female","pwd":"mylu"})
-#     print(res.json())
-#     assert res.status_code == 200
+def test_update_posts(authorized_client):
+    res = authorized_client.put("/up_userprofile/",json = {"firstname":"Mercy","lastname":"Angel","phone":"8778451528","email":"mercy@gmail.com","dob":"2000-08-04","gender":"female","pwd":"mylu"})
+    print(res.json())
+    assert res.status_code == 200
    
-# def test_delete_posts(authorized_client):
-#     res = authorized_client.delete("/del_userprofile/")
-#     assert res.status_code == 204
+def test_delete_posts(authorized_client):
+    res = authorized_client.delete("/del_userprofile/")
+    assert res.status_code == 204
     
